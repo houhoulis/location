3327e04551be2a6318c29daaf0ab89909a37d5f0
Chris Houhoulis <chris.houhoulis@dockyard.com>
2025-02-05 15:17:20 -0500

    Personal preferences: default in dev.exs to timestamps & port 4001.

--- config/dev.exs
+++ config/dev.exs
@@ -9,7 +9,7 @@ import Config
 # Binding to loopback ipv4 address prevents access from other machines.
 config :location, LocationWeb.Endpoint,
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 4001],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
@@ -56,7 +56,7 @@ config :location, LocationWeb.Endpoint,
 config :location, dev_routes: true
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, format: "$time [$level] $message\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
