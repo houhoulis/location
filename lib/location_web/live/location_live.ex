00802b87432bc6dd9b8f1f0aa146809f27736073
Chris Houhoulis <chris.houhoulis@dockyard.com>
2025-02-10 15:09:32 -0500

    Replace Google embed with uMap example and OpenStreetMap embed.

--- lib/location_web/live/location_live.ex
+++ lib/location_web/live/location_live.ex
@@ -4,7 +4,17 @@ defmodule LocationWeb.LocationLive do
   require Logger
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, lat: 0, long: 0, accuracy: 0)}
+    {:ok,
+     assign(socket,
+       lat: 0,
+       long: 0,
+       left: -0.0018,
+       lower: -0.001,
+       right: 0.0018,
+       upper: 0.001,
+       accuracy: 0,
+       ready: false
+     )}
   end
 
   def render(assigns) do
@@ -13,16 +23,41 @@ defmodule LocationWeb.LocationLive do
       Hello lat {@lat} and long {@long} with accuracy {@accuracy}!
     </p>
 
-    <iframe
-      src={"https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d737.6649957308954!2d#{@long}2!3d#{@lat}1!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDLCsDE4JzI1LjIiTiA3McKwMDYnMTcuNCJX!5e0!3m2!1sen!2sus!4v1739210693229!5m2!1sen!2sus"}
-      width="600"
-      height="450"
-      style="border:0;"
-      allowfullscreen=""
-      loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"
-    >
-    </iframe>
+    <p>
+      width="100%"
+      height="300px"
+      frameborder="0"
+      allowfullscreen
+      allow="geolocation"
+      src="//umap.openstreetmap.fr/en/map/new/?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true&datalayers=&editinosmControl=true&captionControl=true#18/46.40502/23.93210"
+      >
+    </p>
+    <p>
+      <a href="//umap.openstreetmap.fr/en/map/new/?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true&datalayers=&editinosmControl=true&captionControl=true#18/46.40502/23.93210">
+        See full screen on uMap
+      </a>
+    </p>
+
+    <div :if={!@ready}>
+      <p>
+        Loading current position....
+      </p>
+    </div>
+    <div :if={@ready}>
+      <iframe
+        width="425"
+        height="350"
+        src={"https://www.openstreetmap.org/export/embed.html?bbox=#{@left}%2C#{@lower}%2C#{@right}%2C#{@upper}&layer=mapnik&marker=#{@lat}%2C#{@long}"}
+        style="border: 1px solid black"
+      >
+      </iframe>
+      <br />
+      <small>
+        <a href={"https://www.openstreetmap.org/?mlat=#{@lat}&amp;mlon=#{@long}#map=19/#{@lat}/#{@long}"}>
+          View Larger Map on OpenStreetMap
+        </a>
+      </small>
+    </div>
     """
   end
 
@@ -31,6 +66,16 @@ defmodule LocationWeb.LocationLive do
         %{"coordinates" => %{"latitude" => lat, "longitude" => long, "accuracy" => accuracy}},
         socket
       ) do
-    {:noreply, assign(socket, lat: lat, long: long, accuracy: accuracy)}
+    {:noreply,
+     assign(socket,
+       lat: lat,
+       long: long,
+       left: long - 0.0018,
+       lower: lat - 0.001,
+       right: long + 0.0018,
+       upper: lat + 0.001,
+       accuracy: accuracy,
+       ready: true
+     )}
   end
 end
