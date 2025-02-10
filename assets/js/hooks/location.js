79c08fe14095d2c60cb14860b69f0d79ca68169a
Chris Houhoulis <chris.houhoulis@dockyard.com>
2025-02-10 14:16:55 -0500

    Make location.js terser and get rid of this2.

--- assets/js/hooks/location.js
+++ assets/js/hooks/location.js
@@ -1,18 +1,8 @@
 export default Location = {
   mounted() {
-    const this2 = this;
+    const success = (positionObject) => this.pushEvent("location-update", {coordinates: positionObject.coords})
 
-    function success(position) {
-      const coords = position.coords
-
-      console.debug(`Success! Lat ${coords.latitude}, long ${coords.longitude}, accuracy ${coords.accuracy}.`)
-
-      this2.pushEvent("location-update", {coordinates: coords})
-    }
-
-    function error(err) {
-      console.error(`Error: ${err.code}: ${err.message}.`)
-    }
+    const error = (err) => console.error(`Error: ${err.code}: ${err.message}.`)
 
     const options = {
       enableHighAccuracy: true,
