d452c3f108d3acc31f1a9e89ca34598e1fa3d4eb
Chris Houhoulis <chris.houhoulis@dockyard.com>
2025-02-11 14:03:14 -0500

    Zoom in on embedded map. Cleanup. Fix link.

--- lib/location_web/live/location_live.ex
+++ lib/location_web/live/location_live.ex
@@ -8,10 +8,10 @@ defmodule LocationWeb.LocationLive do
      assign(socket,
        lat: 0,
        long: 0,
-       left: -0.0018,
-       lower: -0.001,
-       right: 0.0018,
-       upper: 0.001,
+       left: -0.0009,
+       lower: -0.0005,
+       right: 0.0009,
+       upper: 0.0005,
        accuracy: 0,
        ready: false
      )}
@@ -20,7 +20,7 @@ defmodule LocationWeb.LocationLive do
   def render(assigns) do
     ~H"""
     <p id="location" phx-hook="Location">
-      Hello lat {@lat} and long {@long} with accuracy {@accuracy}!
+      Hello lat {@lat} and long {@long} with a {@accuracy}-meter margin of error!
     </p>
 
     <div :if={!@ready}>
@@ -28,6 +28,7 @@ defmodule LocationWeb.LocationLive do
         Loading current position....
       </p>
     </div>
+
     <div :if={@ready}>
       <iframe
         width="425"
@@ -38,7 +39,7 @@ defmodule LocationWeb.LocationLive do
       </iframe>
       <br />
       <small>
-        <a href={"https://www.openstreetmap.org/?mlat=#{@lat}&amp;mlon=#{@long}#map=19/#{@lat}/#{@long}"}>
+        <a href={"https://www.openstreetmap.org/?mlat=#{@lat}&mlon=#{@long}#map=19/#{@lat}/#{@long}"}>
           View Larger Map on OpenStreetMap
         </a>
       </small>
@@ -55,11 +56,11 @@ defmodule LocationWeb.LocationLive do
      assign(socket,
        lat: lat,
        long: long,
-       left: long - 0.0018,
-       lower: lat - 0.001,
-       right: long + 0.0018,
-       upper: lat + 0.001,
-       accuracy: accuracy,
+       left: long - 0.0009,
+       lower: lat - 0.0005,
+       right: long + 0.0009,
+       upper: lat + 0.0005,
+       accuracy: Float.round(accuracy, 3),
        ready: true
      )}
   end
